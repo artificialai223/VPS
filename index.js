58011fdd0149a23945079205faba139750749797
RedstoneMiner27 <redstoneminer27@yahoo.com>
2019-03-15 14:39:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ message.channel.send(feedbackLink)
 	const canvas = Canvas.createCanvas(700, 250);
 	const ctx = canvas.getContext('2d');
 
-	const background = await Canvas.loadImage('./wallpaper.jpg');
+	const background = await Canvas.loadImage('./wallpaper.png');
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
 	ctx.strokeStyle = '#74037b';
