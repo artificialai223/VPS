f3a79d58d8192ffd5254ae577e8626a833421cdf
artificialai223 <47569748+artificialai223@users.noreply.github.com>
2019-04-01 17:35:29 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,11 @@
 module.exports = function onReady(bot) {
     if (!bot.user.avatar) {
-        bot.user.setAvatar('./avatar.png').catch('error' + console.error);
+        bot.user.setAvatar('./Crystal').catch('error' + console.error);
     }
     bot.user.setStatus("online");
     bot.user.setAFK(false);
-    bot.user.setUsername("Fortnite Tracker");
-    bot.user.setActivity('Visual Studio Code', {
+    bot.user.setUsername("Crystal");
+    bot.user.setActivity('Managing Servers', {
         url: null,
         type: 'PLAYING'
     });
