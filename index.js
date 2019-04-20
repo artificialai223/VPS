cb510052e8846d80b7074c94480c0b69148490b1
artificialai223 <47569748+artificialai223@users.noreply.github.com>
2019-04-20 10:46:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const fetch = require('node-fetch');
 const auth = require('./auth.json');
 const onReady = require('./events/ready');
-const Discord = require('discord.js')
 const client = new Discord.Client()
 let prefix = 'cr!'
 
@@ -248,97 +247,6 @@ client.once('ready', () => {
 })
    console.log('Crystal Is Online If Errors Happen Contact RedstoneMiner27! Tell Him The Error(s)')
 })
-const fortniteTracker = new Discord.Client();
-
-fortniteTracker.on('ready', () => {
-    onReady(client);
-});
-
-fortniteTracker.on('message', async msg => {
-
-    if (!msg.content.startsWith(config.prefix) || msg.author.Crystal === true) return;
-
-    if (msg.content.startsWith("!fbr")) {
-        const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
-
-        do {
-            if (config.platforms.includes(args[1]) && args.length === 3) {
-                msg.channel.startTyping();
-
-                try {
-                    let platformSelected = args[1]
-                    let userSelected = args[2].toLowerCase();
-                    let user = await getUserByPlatform(userSelected, platformSelected);
-                    let json = await user.json();
-                    console.log(json);
-
-
-                    if (json.error) {
-                        throw new Error('Player not Found');
-                    }
-
-                    const responseObject = {
-                        "ayy": "Ayy, lmao!",
-                        "wat": "Say what?",
-                        "lol": "roflmaotntpmp"
-                    };
-                    let found = json.lifeTimeStats.find((el) => el.key === 'Wins')
-                    // msg.reply(responseObject);
-                    setStats(msg);
-                    msg.channel.stopTyping();
-
-                } catch (error) {
-                    msg.reply(`:x: ${error}`);
-                    msg.channel.stopTyping();
-                }
-                break;
-            }
-        } while (0)
-    }
-});
-
-const getUserByPlatform = (user, platform = config.platforms[0]) => {
-        return fetch(`https://api.fortnitetracker.com/v1/profile/${platform}/${user}`, {
-            method: 'GET',
-            headers: {
-                'TRN-Api-Key': 296d3ab6-8259-4a3c-afeb-96a3c778a1c4
-            },
-        })
-        .then(res => res)
-        .catch(error => false)
-}
-
-function setStats(msg) {
-    const embed = new Discord.RichEmbed()
-        .setTitle("Managing 1000 Servers")
-        .setAuthor("Stormer#5620")
-        /*
-         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-         */
-        .setColor(0x00AE86)
-        .setDescription("")
-        .setFooter("")
-        .setImage("http://i.imgur.com/yVpymuV.png")
-        .setThumbnail("http://i.imgur.com/p2qNFag.png")
-        /*
-         * Takes a Date object, defaults to current date.
-         */
-        .setTimestamp()
-        .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-        .addField("",
-            "")
-        /*
-         * Inline fields may not display as inline if the thumbnail and/or image is too big.
-         */
-        .addField("", true)
-        /*
-         * Blank field, useful to create some space.
-         */
-        .addBlankField(true)
-        .addField("", true);
-
-    msg.channel.send({ embed });
-}
 /*
 if (!developer.includes(l.OK) || !developer.includes(CHECK.i + '/9GC')) {
     return console.log(s)
